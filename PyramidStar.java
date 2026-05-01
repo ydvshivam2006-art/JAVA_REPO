e6b983f7ba12ec2235a70b3a36624883d8b31106
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-02 01:44:17 +0530

    PyramidStar

---
+++ PyramidStar.java
@@ -0,0 +1,11 @@
+public class PyramidStar {
+    public static void PyramidStar(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) System.out.print("* ");
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        PyramidStar(5);
+    }
+}

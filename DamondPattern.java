d4de388fcbe3aff8fcd4af6944c08630e25288ad
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-02 01:30:07 +0530

    DamondPattern

---
+++ DamondPattern.java
@@ -0,0 +1,70 @@
+import java.io.*;
+
+public class DamondPattern  {
+    public static void main(String[] args)
+    {
+
+        
+        int number = 7;
+
+        
+        int m = 1;
+
+    
+        int n;
+
+
+        while (m <= number) {
+            n = 1;
+
+            
+            while (n++ <= number - m) {
+
+                System.out.print(" ");
+            }
+
+            n = 1;
+
+            while (n++ <= m * 2 - 1) {
+
+            
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+            
+            m++;
+        }
+
+        
+        m = number - 1;
+
+    
+        while (m > 0) {
+            n = 1;
+
+            
+            while (n++ <= number - m) {
+
+                
+                System.out.print(" ");
+            }
+
+            n = 1;
+
+            
+            while (n++ <= m * 2 - 1) {
+
+            
+                System.out.print("*");
+            }
+
+        
+            System.out.println();
+
+        
+            m--;
+        }
+    }
+}

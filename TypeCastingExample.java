e44557da6c704f2bfcce5a2c502946f3444aca80
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-02 02:29:44 +0530

    TypeCastingExample

---
+++ TypeCastingExample.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class TypeCastingExample
+{
+
+
+    public static void main(String[] args)
+    {
+        
+        int i = 97;
+
+        
+        char ch = (char)i;
+
+        
+        System.out.println(ch);
+    }
+}

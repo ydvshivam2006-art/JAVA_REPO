5d83162b7a452ef4f69ce69ec90bb8e3208ac104
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-02 02:34:23 +0530

    CharToInt

---
+++ CharToInt.java
@@ -0,0 +1,20 @@
+class CharToInt
+{
+    
+    public static void main(String[] args)
+    {
+
+        char num = '3';
+
+        
+        System.out.println("char value: " + num);
+
+
+
+        int n = num - '0';
+        
+
+    
+        System.out.println("int value: " + n);
+    }
+}

18df179ff42be34d36249f81e4c923bfc5e1d8a8
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-02 02:01:49 +0530

    BooleanToStringExample

---
+++ BooleanToStringExample.java
@@ -0,0 +1,15 @@
+ 
+class BooleanToStringExample
+ { 
+	public static void main(String[] args) 
+	{ 
+		
+		boolean value = true; 
+
+	
+		String output = Boolean.toString(value); 
+
+		
+		System.out.println(output); 
+	} 
+}

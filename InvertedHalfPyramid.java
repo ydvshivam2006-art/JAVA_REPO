5ce73be354fca59bf2f159435f29bedb09bab75c
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-02 01:04:19 +0530

    InvertedHalfPyramid

---
+++ InvertedHalfPyramid.java
@@ -0,0 +1,11 @@
+public classInvertedHalfPyramid  {
+    public static void main(String[] args) {
+        int rows = 9;
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

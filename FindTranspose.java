7791a0642897618e92bfa1fe704a77cb97f8414f
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-02 19:18:04 +0530

    FindTranspose

---
+++ FindTranspose.java
@@ -0,0 +1,26 @@
+public class FindTranspose {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] transpose = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        System.out.println("Transpose Matrix:");
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                System.out.print(transpose[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}

3e55fb79829bfc45f044c7dd3ae85f34c7566a45
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-02 18:38:14 +0530

    MergeArrays

---
+++ MergeArrays.java
@@ -0,0 +1,8 @@
+public class MergeArrays {
+    public static int[] merge(int[] a, int[] b) {
+        int[] result = new int[a.length + b.length];
+        System.arraycopy(a, 0, result, 0, a.length);
+        System.arraycopy(b, 0, result, a.length, b.length);
+        return result;
+    }
+}

6d50a74b94c9474375402b4c8660139b7a8c387c
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-02 17:58:51 +0530

    SimpleInterest

---
+++ SimpleInterest.java
@@ -0,0 +1,15 @@
+
+import java.io.*;
+
+class       SimpleInterest
+ {
+    public static void main(String args[])
+    {
+        
+        float P = 1, R = 1, T = 1;
+
+        
+        float SI = (P * T * R) / 100;
+        System.out.println("Simple interest = " + SI);
+    }
+}

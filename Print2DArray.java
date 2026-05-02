ed4de3cb75ee2117ee151d4e65278721d43dc795
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-02 19:06:20 +0530

    Print2DArray

---
+++ Print2DArray.java
@@ -0,0 +1,17 @@
+public class Print2DArray {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("2D Array elements:");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}

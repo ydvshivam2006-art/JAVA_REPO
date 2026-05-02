b07a1038cce8138d7369cb1391c87f58d0171979
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-02 18:16:40 +0530

    LargestElement

---
+++ LargestElement.java
@@ -0,0 +1,13 @@
+public class LargestElement {
+    public static void main(String[] args) {
+        int[] arr = {25, 11, 7, 75, 56};
+        int max = arr[0];
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Largest element in given array: " + max);
+    }
+}

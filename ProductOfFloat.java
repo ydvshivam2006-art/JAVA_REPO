d3b74bb87d08504013c52c15dc49406525d2224d
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-02 17:46:10 +0530

    ProductOfFloat

---
+++ ProductOfFloat.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class  ProductOffloat
+{
+    public static void main(String[] args)
+    {
+
+        
+        float f1 = 1.5f;
+        float f2 = 2.0f;
+
+        float p = f1 * f2;
+
+        
+        System.out.println("The product is: " + p);
+    }
+}

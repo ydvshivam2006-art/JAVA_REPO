1c9cab5cc79fdd6141d9755afdb8a93c87665247
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-03 21:30:07 +0530

    InitializeList

---
+++ InitializeList.java
@@ -0,0 +1,11 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class InitializeList {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(Arrays.asList("Apple", "Banana", "Cherry"));
+        System.out.println(list);
+    }
+}

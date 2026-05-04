aa59cd2d6e7ce5c2049827ae02900edcedba3813
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-04 11:27:15 +0530

    Main

--- Main.java
+++ Main.java
@@ -1,35 +1,37 @@
-// First Interface
-interface Interface1 {
-    void captureClick();   // click capture
+interface Printer {
+    void printing();
 }
 
-// Second Interface
-interface Interface2 {
-    void captureVideo();   // video / second action capture
+interface Scanner {
+    void scanning();
 }
 
-// Class implementing both interfaces
-class MyApp implements Interface1, Interface2 {
+interface Fax {
+    void faxing();
+}
 
+class AllInOne implements Printer, Scanner, Fax {
     @Override
-    public void captureClick() {
-        System.out.println("Click captured (Capture 1)");
+    public void printing() {
+        System.out.println("Printing document...");
     }
 
     @Override
-    public void captureVideo() {
-        System.out.println("Video captured (Capture 2)");
+    public void scanning() {
+        System.out.println("Scanning document...");
+    }
+
+    @Override
+    public void faxing() {
+        System.out.println("Sending fax...");
     }
 }
 
-// Main class
 public class Main {
     public static void main(String[] args) {
-
-        MyApp obj = new MyApp();
-
-        // Calling both methods
-        obj.captureClick();   // Capture 1
-        obj.captureVideo();   // Capture 2
+        AllInOne device = new AllInOne();
+        device.printing();
+        device.scanning();
+        device.faxing();
     }
 }

6178e1258ba9f12d6cfd588c537d96a9f9f2c61e
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-07 11:09:55 +0530

    CollectionSize

---
+++ CollectionSize.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class CollectionSize {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        
+        System.out.println("Size of collection: " + list.size());
+    }
+}

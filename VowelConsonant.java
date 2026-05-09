3b26701d845eac4b1e4e0e91c1f50e8f329fecae
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-09 19:28:19 +0530

    VowelConsonant

---
+++ VowelConsonant.java
@@ -0,0 +1,8 @@
+public class VowelConsonant {
+    public static void main(String[] args) {
+        char ch = 'e';
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            System.out.println("Vowel");
+        else System.out.println("Consonant");
+    }
+}

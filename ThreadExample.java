aed1e06f28c27a329d397a17310e72c69ab83c32
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-11 23:49:43 +0530

    ThreadExample

---
+++ ThreadExample.java
@@ -0,0 +1,12 @@
+class MyThread extends Thread {
+    public void run() {
+        System.out.println("Thread is running...");
+    }
+}
+
+public class ThreadExample {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.start();
+    }
+}

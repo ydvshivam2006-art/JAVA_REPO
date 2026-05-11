d2983f44142f1c284a6322d2447c4916a688f1d9
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-11 23:40:33 +0530

    LinearSearch

---
+++ LinearSearch.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static int search(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] data = {10, 20, 30, 40, 50};
+        int target = 30;
+        int result = search(data, target);
+        System.out.println(result != -1 ? "Element found at index: " + result : "Element not found");
+    }
+}

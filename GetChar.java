53129c10806cda33b2c9387be8ae5a2175813452
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-14 10:10:44 +0530

    GetChar

---
+++ GetChar.java
@@ -0,0 +1,8 @@
+public class GetChar {
+    public static void main(String[] args) {
+        String str = "GeeksForGeeks";
+        int index = 5;
+        char ch = str.charAt(index);
+        System.out.println("Character at index " + index + " is: " + ch);
+    }
+}

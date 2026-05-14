a78b28aa362b70c5da9caa25b910e8c7a545c784
Shivam Yadav <ydvshivam2006@gmail.com>
2026-05-14 10:22:36 +0530

    PangramCheck

---
+++ PangramCheck.java
@@ -0,0 +1,14 @@
+public class PangramCheck {
+    public static void main(String[] args) {
+        String str = "The quick brown fox jumps over the lazy dog";
+        boolean[] alphabetPresent = new boolean[26];
+        int count = 0;
+        for (char c : str.toLowerCase().toCharArray()) {
+            if (c >= 'a' && c <= 'z' && !alphabetPresent[c - 'a']) {
+                alphabetPresent[c - 'a'] = true;
+                count++;
+            }
+        }
+        System.out.println("Is Pangram: " + (count == 26));
+    }
+}
